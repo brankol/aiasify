971ff85ab781a791e61b449802a07643cfb6c65c
Branko Kolesaric <branko@kolesaric.me>
2015-06-21 01:48:25 +0200

    Add request function

--- src/aiasify.js
+++ src/aiasify.js
@@ -1,3 +1,9 @@
+/**
+* A declarative, modular approach to ajaxifying your website.
+*
+* @namespace aiasify
+*/
+
 (function(root, factory) {
     'use strict';
     if (typeof define === 'function' && define.amd) {
@@ -10,5 +16,39 @@
 }(this, function() {
     'use strict';
 
-    return {};
+    /**
+    * Initiates a new XMLHttpRequest.
+    *
+    * @param {string} type The HTTP verb.
+    * @param {string} url The url for the XHR request.
+    * @param {object} data Optional. The data to be passed with a POST or PUT request.
+    *
+    * @memberof aiasify
+    */
+    function request(config) {
+        config = config || {};
+        var req = new XMLHttpRequest();
+        var settings = {
+            type: config.type || 'GET',
+            url: config.url || window.location.href
+        };
+
+        req.open(settings.type, settings.url);
+        req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        req.onreadystatechange = function () {
+            if (this.readyState === 4) {
+                if (this.status === 200) {
+                    console.log(req.responseText);
+                }
+                else {
+                    throw new Error('Request responded with status ' + req.statusText);
+                }
+            }
+        };
+        req.send(config.data);
+    }
+
+    return {
+        request: request
+    };
 }));
